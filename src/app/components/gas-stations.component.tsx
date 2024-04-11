aa09ec536a41fa47b1f721f9ed395ca7190172e7
Rafael Campos <rafaelsilvamz111@gmail.com>
2024-04-11 15:56:24 -0300

    feat: adjust mock

--- src/app/components/gas-stations.component.tsx
+++ src/app/components/gas-stations.component.tsx
@@ -2,17 +2,24 @@
 
 import { gasStationIcon } from '@/app/icons/gas-station.icon'
 import { Marker } from '@/app/lib/react-leaflet'
-import { gasStationCoords } from '@/app/mocks/gasStationMock'
 import { favoriteGasStationIcon } from '../icons/favorite-gas-station'
+import { postos } from '../mocks/gasStationMock'
 
 export const GasStations = () => {
-  const [firstCoord, ...restOfCoords] = gasStationCoords
+  const [firstCoord, ...restOfCoords] = postos.postos
 
   return (
     <>
-      <Marker icon={favoriteGasStationIcon} position={firstCoord} />
+      <Marker
+        icon={favoriteGasStationIcon}
+        position={{ lat: firstCoord.lat, lng: firstCoord.lng }}
+      />
       {restOfCoords.map((coords, index) => (
-        <Marker key={index} icon={gasStationIcon} position={coords} />
+        <Marker
+          key={index}
+          icon={gasStationIcon}
+          position={{ lat: coords.lat, lng: coords.lng }}
+        />
       ))}
     </>
   )
