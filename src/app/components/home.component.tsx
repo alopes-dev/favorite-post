3cc9ba2bb92fd87c4d89f511890076a1ec5a5a02
Antonio Lopes-(alopes.dev) <pdkdidy2011@gmail.com>
2024-06-11 19:32:33 +0100

    d

--- src/app/components/home.component.tsx
+++ src/app/components/home.component.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useMemo } from 'react'
+import { useCallback, useEffect, useMemo } from 'react'
 import { checkFavoritePost } from '../mocks/checkFavoritePost'
 import { MapProvider } from '../providers/map.provider'
 import FavoritePostListPage from './favorite.component'
@@ -9,12 +9,21 @@ import { GasStations } from './gas-stations.component'
 import { Map } from './map.component'
 import { MyLocation } from './my-location.component'
 import { Position } from './position.component'
+import { getAllPosts } from '../services/get-all-posts'
 
 export default function Home() {
   const hasPostoFavorito = useMemo(
     () => checkFavoritePost().PostoFavoritoID,
     [],
   )
+
+  const getSome = useCallback(async () => {
+    await getAllPosts()
+  }, [])
+
+  useEffect(() => {
+    getSome()
+  }, [getSome])
 
   if (!hasPostoFavorito) return <FavoritePostListPage />
 
--- src/app/services/get-all-posts.ts
+++ src/app/services/get-all-posts.ts
@@ -1,1 +1,14 @@
-export const getAllPosts = async () => {}
+import axios from 'axios'
+
+export const getAllPosts = async () => {
+  try {
+    const rest = await axios.get(
+      'https://api-sandbox.kmdevantagens.com.br/kmv/api/v1/posto?geolocalizacao=lat:-20.9074473%7Clng:-49.2661772%7Cdistancia:3&servico=PLANO_MKT:S&client_id=6ac535bf2cd146b7bd53cb23dd7222fd&client_secret=1C9aAbbf075B497085CDa6616008dD35',
+    )
+
+    console.log(rest)
+    console.log(rest)
+  } catch (error) {
+    console.log(error)
+  }
+}
